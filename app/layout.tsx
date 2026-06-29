1be0726c965951027e7b472f09b359afd53cea33
ArkalKeirosRas <recargamarcas6@gmail.com>
2026-06-29 11:36:50 -0600

    nothing

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           (adsbygoogle = window.adsbygoogle || []).push({});
         `}</Script>
             <body>
+      
         {children}    
         
       </body>
